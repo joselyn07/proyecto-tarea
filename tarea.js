6f00207ade600d61e3ac44f152318de2d24ede82
joselyn07 <jnunezg@fwdcostarica.com>
2023-07-26 15:46:05 -0600

    actualizar tarea

--- tarea.js
+++ tarea.js
@@ -1,19 +1,46 @@
-const tarea= document.getElementById('tarea');
-const btn= document.getElementById('btn');
-const empty= document.querySelector('.empty');
-
-
-
-
-console.log(tarea.value)
-
-
-btn.addEventListener('click', function(){
-    const cajaTareas= document.getElementById('cajaTareas');
-    console.log(tarea.value)
-    cajaTareas.innerHTML= tarea.value
-})
-
 confirm("ingrese un texto");
 
+const tarea= document.getElementById('tarea');
+const btn= document.getElementById('btn');
+const color=document.getElementById('color')
+console.log(tarea.value)
+
+btn.addEventListener('click', function(){
+    if (verificar(tarea.value)) {
+        const cajaTareas= document.getElementById('cajaTareas');
+        console.log(tarea.value)
+        const itemCaja=document.createElement('div');
+        itemCaja.className='contenedorTarea';
+        itemCaja.id = 'contenedorTarea'
+        itemCaja.innerHTML= `
+        <div id='Tarea'>
+        <input type="checkbox" id="uno">
+        <label class='task' for="uno">${tarea.value}</label>
+        </div>
+        <button onclick="eliminar()" onclick="myFunction()">eliminar</button>
+        `;
+        cajaTareas.appendChild(itemCaja);
+    }else{
+        alert('La tarea ya existe')
+    }
+    
+
+})
+
+function eliminar(){
+    const tarea = document.getElementById('contenedorTarea')
+    console.log(tarea)
+
+    tarea.remove()
+}
+
+function verificar(task){
+    let tasks= document.querySelectorAll('.task')
+    for (let contador = 0; contador < tasks.length; contador++) {
+        if (tasks[contador].textContent===task) {
+            return false
+        }
+    }
+    return true
+}
 
